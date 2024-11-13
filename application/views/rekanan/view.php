ae06a429d4901958812bb957c2fc6f89cde57b6c
kelvin kusmawan <kelvinkusmawan12@gmail.com>
2024-11-13 12:58:29 +0700

    tb_rekanan

---
+++ application/views/rekanan/view.php
@@ -0,0 +1,103 @@
+<div class="container-xl">
+    <div class="row font-kecil">
+        <div class="col-6">
+            <div class="mb-1 row">
+                <label class="col-3 col-form-label required">Kode</label>
+                <div class="col">
+                    <input type="hidden" class="form-control font-kecil" name="id" id="id" value="<?= $data['id']; ?>">
+                    <input type="text" class="form-control font-kecil" name="kode" id="kode" placeholder="Kode" value="<?= $data['kode']; ?>" readonly>
+                </div>
+            </div>
+            <div class=" mb-1 row">
+                <label class="col-3 col-form-label required">Inisial</label>
+                <div class="col">
+                    <input type="text" class="form-control font-kecil" name="inisial" id="inisial" placeholder="Inisial" value="<?= $data['inisial']; ?>" readonly>
+                </div>
+            </div>
+            <div class=" mb-1 row">
+                <label class="col-3 col-form-label required">Nama Rekanan</label>
+                <div class="col">
+                    <input type="text" class="form-control font-kecil" name="nama_rekanan" id="nama_rekanan" placeholder="Nama Rekanan" value="<?= $data['nama_rekanan']; ?>" readonly>
+                </div>
+            </div>
+            <div class=" mb-1 row">
+                <label class="col-3 col-form-label required">Alamat Rekanan</label>
+                <div class="col">
+                    <input type="text" class="form-control font-kecil" name="alamat_rekanan" id="alamat_rekanan" placeholder="Alamat Rekanan" value="<?= $data['alamat_rekanan']; ?>" readonly>
+                </div>
+            </div>
+            <div class=" mb-1 row">
+                <label class="col-3 col-form-label required">Kode Pos</label>
+                <div class="col">
+                    <input type="text" class="form-control font-kecil" name="kodepos" id="kodepos" placeholder="Kode Pos" value="<?= $data['kodepos']; ?>" re>
+                </div>
+            </div>
+            <div class="mb-1 row">
+                <label class="col-3 col-form-label required">Telp</label>
+                <div class="col">
+                    <input type="text" class="form-control font-kecil" name="telp" id="telp" placeholder="Telp" value="<?= $data['telp']; ?>" readonly>
+                </div>
+            </div>
+            <div class="mb-1 row">
+                <label class="col-3 col-form-label required">Fax</label>
+                <div class="col">
+                    <input type="text" class="form-control" name="fax" id="fax" placeholder="Fax" value="<?= $data['fax']; ?>" readonly>
+                </div>
+            </div>
+
+
+        </div>
+        <div class="col-6">
+            <div class="mb-1 row">
+                <label class="col-3 col-form-label required">Email</label>
+                <div class="col">
+                    <input type="text" class="form-control font-kecil" name="email" id="email" placeholder="Email" value="<?= $data['email']; ?>" readonly>
+                </div>
+            </div>
+
+            <div class="mb-1 row">
+                <label class="col-3 col-form-label required">Npwp</label>
+                <div class="col">
+                    <input type="text" class="form-control font-kecil" name="npwp" id="npwp" placeholder="Npwp" value="<?= $data['npwp']; ?>" readonly>
+                </div>
+            </div>
+
+            <div class="mb-1 row">
+                <label class="col-3 col-form-label required">Bank</label>
+                <div class="col">
+                    <input type="text" class="form-control font-kecil" name="bank" id="bank" placeholder="Bank" value="<?= $data['bank']; ?>" readonly>
+                </div>
+            </div>
+            <div class="mb-1 row">
+                <label class="col-3 col-form-label required">Rek BANK</label>
+                <div class="col">
+                    <input type="text" class="form-control font-kecil" name="rek_bank" id="rek_bank" placeholder="Rek BANK" value="<?= $data['rek_bank']; ?>" readonly>
+                </div>
+            </div>
+            <div class="mb-1 row">
+                <label class="col-3 col-form-label required">Atas Nama BANK</label>
+                <div class="col">
+                    <input type="text" class="form-control font-kecil" name="an_bank" id="an_bank" placeholder="Atas Nama BANK" value="<?= $data['an_bank']; ?>" readonly>
+                </div>
+            </div>
+            <div class="mb-1 row">
+                <label class="col-3 col-form-label required">Kontak</label>
+                <div class="col">
+                    <input type="text" class="form-control font-kecil" name="kontak" id="kontak" placeholder="Kontak" value="<?= $data['kontak']; ?>" readonly>
+                </div>
+            </div>
+            <div class="mb-1 row">
+                <label class="col-3 col-form-label required">Jabatan</label>
+                <div class="col">
+                    <input type="text" class="form-control font-kecil" name="jabatan" id="jabatan" placeholder="Jabatan" value="<?= $data['jabatan']; ?>" readonly>
+                </div>
+            </div>
+            <div class="mb-1 row">
+                <label class="col-3 col-form-label required">No KTP</label>
+                <div class="col">
+                    <input type="text" class="form-control font-kecil" name="noktp" id="noktp" placeholder="No KTP" value="<?= $data['noktp']; ?>" readonly>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
