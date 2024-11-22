00fdc119d7a8050c41a7a979b6b66609e446da0e
kelvin kusmawan <kelvinkusmawan12@gmail.com>
2024-11-22 09:50:01 +0700

    bckeluar

--- application/views/bckeluar/bckeluar.php
+++ application/views/bckeluar/bckeluar.php
@@ -6,7 +6,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <div class="row g-0 d-flex align-items-between">
       <div class="col-md-6">
         <h2 class="page-title p-2">
-            DOK BC KELUAR
+          DOK BC KELUAR
         </h2>
       </div>
       <div class="col-md-6" style="text-align: right;">
@@ -24,11 +24,21 @@ defined('BASEPATH') or exit('No direct script access allowed');
             <div class="col-sm-5 d-flex">
               <select class="form-control form-sm font-kecil font-bold mr-1 bg-teal text-white form-select" id="jns_bc" name="jns_bc">
                 <!-- <option value="X">Pilih</option> -->
-                <option value="Y" <?php if($this->session->userdata('jnsbc')=='Y'){ echo "selected"; } ?>>All</option>
-                <option value="25" <?php if($this->session->userdata('jnsbc')=='25'){ echo "selected"; } ?>>BC 2.5</option>
-                <option value="261" <?php if($this->session->userdata('jnsbc')=='261'){ echo "selected"; } ?>>BC 2.6.1</option>
-                <option value="30" <?php if($this->session->userdata('jnsbc')=='30'){ echo "selected"; } ?>>BC 3.0</option>
-                <option value="41" <?php if($this->session->userdata('jnsbc')=='41'){ echo "selected"; } ?>>BC 4.1</option>
+                <option value="Y" <?php if ($this->session->userdata('jnsbc') == 'Y') {
+                                    echo "selected";
+                                  } ?>>All</option>
+                <option value="25" <?php if ($this->session->userdata('jnsbc') == '25') {
+                                      echo "selected";
+                                    } ?>>BC 2.5</option>
+                <option value="261" <?php if ($this->session->userdata('jnsbc') == '261') {
+                                      echo "selected";
+                                    } ?>>BC 2.6.1</option>
+                <option value="30" <?php if ($this->session->userdata('jnsbc') == '30') {
+                                      echo "selected";
+                                    } ?>>BC 3.0</option>
+                <option value="41" <?php if ($this->session->userdata('jnsbc') == '41') {
+                                      echo "selected";
+                                    } ?>>BC 4.1</option>
               </select>
               <input type="text" class="form-control form-sm font-kecil font-bold mr-1 tglpilih" title="Tanggal Awal" id="tglawal" name="tglawal" style="width: 95px;" value="<?= $tglawal; ?>">
               <span class="mt-2 text-blue"> ~ </span>
@@ -36,8 +46,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
               <a href="#" class="btn btn-success btn-sm font-bold" id="updatebckeluar"><i class="fa fa-refresh"></i><span class="ml-1">UPDATE</span></a>
             </div>
             <div class="col-sm-7 d-flex flex-row-reverse" style="text-align: right;">
-              <a href="#" class="btn btn-danger btn-sm font-bold" id="topdf"><i class="fa fa-file-excel-o"></i><span class="ml-1">Export PDF</span></a>
-              <a href="<?= base_url().'bcmasuk/excel'; ?>" class="btn btn-success btn-sm font-bold mr-1" id="toexcel"><i class="fa fa-file-pdf-o"></i><span class="ml-1">Export Excel</span></a>
+              <a href="<?= base_url('bckeluar/cetakpdf'); ?>" class="btn btn-danger btn-sm font-bold" id="topdf"><i class="fa fa-file-excel-o"></i><span class="ml-1">Export PDF</span></a>
+              <a href="<?= base_url() . 'bckeluar/excel'; ?>" class="btn btn-success btn-sm font-bold mr-1" id="toexcel"><i class="fa fa-file-pdf-o"></i><span class="ml-1">Export Excel</span></a>
             </div>
           </div>
           <div class="card card-active" style="clear:both;">
@@ -53,7 +63,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
                       </select>
                     </div>
                   </span> -->
-                       
+
                 </div>
                 <div class="col-3 ">
 
@@ -101,17 +111,27 @@ defined('BASEPATH') or exit('No direct script access allowed');
               </tr>
             </thead>
             <tbody class="table-tbody" id="body-table" style="font-size: 13px !important;">
-              <?php  $cntbrg=0; $jmpcs=0; $jmkgs=0; $no=0; if($data!=null): foreach ($data->result_array() as $detail) { ?>
-                <tr>
-                  <td class="text-center align-middle"><?= 'BC. '.$detail['jns_bc']; ?></td>
-                  <td class="text-left font-bold font-roboto" style="line-height: 14px;"><a href="<?= base_url().'bckeluar/viewdetail/'.$detail['idx']; ?>" data-bs-toggle='offcanvas' data-bs-target='#canvasdet' data-title='Nomor AJU <?= generatekodebc($detail['jns_bc'],$detail['tgl_aju'],$detail['nomor_aju']); ?>' title='Detail dokumen'><?= $detail['nomor_bc']; ?><br><?= $detail['tgl_bc']; ?></a></td>
-                  <td class="text-left" style="line-height: 14px;"><?= $detail['nomor_sj']; ?><br><?= $detail['tgl_sj']; ?></td>
-                  <td class="text-left"><?= ucwords(strtolower(trim($detail['nama_customer']))); ?><?php if($detail['port']!=''){ echo '- '.ucwords(strtolower($detail['port']));} ?></td>
-                  <td class="text-left"  style="line-height: 14px;"><?= $detail['jml_kemasan'].' '.$detail['kemasan']; ?><br><span class="badge badge-outline text-pink"><?= rupiah($detail['netto'],2).' Kgs'; ?></span></td>
-                  <td class="text-left"><?= $detail['nomor_sppb']; ?></td>
-                  <td class="text-left"><?= $detail['tgl_sppb']; ?></td>
-                </tr>
-              <?php $cntbrg++; $jmpcs += $detail['pcs']; $jmkgs += $detail['kgs']; } endif; ?>
+              <?php $cntbrg = 0;
+              $jmpcs = 0;
+              $jmkgs = 0;
+              $no = 0;
+              if ($data != null) : foreach ($data->result_array() as $detail) { ?>
+                  <tr>
+                    <td class="text-center align-middle"><?= 'BC. ' . $detail['jns_bc']; ?></td>
+                    <td class="text-left font-bold font-roboto" style="line-height: 14px;"><a href="<?= base_url() . 'bckeluar/viewdetail/' . $detail['idx']; ?>" data-bs-toggle='offcanvas' data-bs-target='#canvasdet' data-title='Nomor AJU <?= generatekodebc($detail['jns_bc'], $detail['tgl_aju'], $detail['nomor_aju']); ?>' title='Detail dokumen'><?= $detail['nomor_bc']; ?><br><?= $detail['tgl_bc']; ?></a></td>
+                    <td class="text-left" style="line-height: 14px;"><?= $detail['nomor_sj']; ?><br><?= $detail['tgl_sj']; ?></td>
+                    <td class="text-left"><?= ucwords(strtolower(trim($detail['nama_customer']))); ?><?php if ($detail['port'] != '') {
+                                                                                                        echo '- ' . ucwords(strtolower($detail['port']));
+                                                                                                      } ?></td>
+                    <td class="text-left" style="line-height: 14px;"><?= $detail['jml_kemasan'] . ' ' . $detail['kemasan']; ?><br><span class="badge badge-outline text-pink"><?= rupiah($detail['netto'], 2) . ' Kgs'; ?></span></td>
+                    <td class="text-left"><?= $detail['nomor_sppb']; ?></td>
+                    <td class="text-left"><?= $detail['tgl_sppb']; ?></td>
+                  </tr>
+              <?php $cntbrg++;
+                  $jmpcs += $detail['pcs'];
+                  $jmkgs += $detail['kgs'];
+                }
+              endif; ?>
             </tbody>
           </table>
         </div>
@@ -140,11 +160,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
                 </span>
               </div>
               <div class="col-3" style="line-height: 5px !important;">
-                <?php if($this->session->userdata('invharga')): ?>
-                <h4 class="mb-0 font-kecil font-bold">Grand Total</h4>
-                <span class="font-kecil text-green font-bold">
-                  Rp. <?= rupiah($grandtotal, 2); ?>
-                </span>
+                <?php if ($this->session->userdata('invharga')) : ?>
+                  <h4 class="mb-0 font-kecil font-bold">Grand Total</h4>
+                  <span class="font-kecil text-green font-bold">
+                    Rp. <?= rupiah($grandtotal, 2); ?>
+                  </span>
                 <?php endif; ?>
               </div>
               <div class="col-2">
