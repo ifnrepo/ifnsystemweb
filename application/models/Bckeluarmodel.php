1e77cddc5d6741e1db4afe26123f2d323f46d212
atepasep <atep.saprudin86@gmail.com>
2024-12-07 10:43:30 +0700

    report Filterbc Modif

--- application/models/Bckeluarmodel.php
+++ application/models/Bckeluarmodel.php
@@ -66,6 +66,8 @@ class bckeluarmodel extends CI_Model
 
         if ($this->session->userdata('jnsbc') != 'Y') {
             $this->db->where("tb_header.jns_bc", $this->session->userdata('jnsbc'));
+        }else{
+            $this->db->where_in("tb_header.jns_bc", [25, 30, 261, 41]);
         }
 
         $this->db->where('tb_header.data_ok', 1);
