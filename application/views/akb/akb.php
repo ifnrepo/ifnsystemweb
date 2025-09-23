89e26f3e86a6546d2f5971bff3efaf3aed11ae4b
atepasep <atep.saprudin86@gmail.com>
2025-09-23 07:55:11 +0700

    Tes Publish

--- application/views/akb/akb.php
+++ application/views/akb/akb.php
@@ -64,8 +64,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
                 </div>
                 <div class="col-5">
                   <span class="mb-1 font-kecil text-primary font-bold">Jumlah Record : <?= $jumlahrek->num_rows(); ?></span><br>
-                  <?php $jmlrek = $jumlahrek->row_array(); $jumlahpcs=0; if(isset($jmlrek['jumlahitemnya'])) { $jumlahpcs = $jmlrek['jumlahitemnya']; } ?>
-                  <span class="mb-1 font-kecil text-primary font-bold">Jumlah Detail : <?= rupiah($jumlahpcs,0).' Item' ?></span>
+                  <?php $jmlrek = $jumlahrek->row_array(); $jumlahpce=0; if(isset($jmlrek['jumlahitemnya'])) { $jumlahpce = $jmlrek['jumlahitemnya']; } ?>
+                  <span class="mb-1 font-kecil text-primary font-bold">Jumlah Detail : <?= rupiah($jumlahpce,0).' Item' ?></span>
                 </div>
                 <div class="col-2">
                   <h4 class="mb-1">
