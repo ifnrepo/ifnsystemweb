91ef7c4dd0c83b4b18344f527351517ac75411e7
kelvin kusmawan <kelvinkusmawan12@gmail.com>
2025-10-08 12:37:22 +0700

    getdata_bc261-261

--- application/models/Bckeluarmodel.php
+++ application/models/Bckeluarmodel.php
@@ -7,26 +7,44 @@ class bckeluarmodel extends CI_Model
         $tglakhir = $this->session->userdata('tglakhir');
         $jnsbc = $this->session->userdata('jnsbc');
 
-        $this->db->select('tb_header.*,customer.*,sum(tb_detail.pcs) as pcs,sum(tb_detail.kgs) as kgs,tb_header.id as idx,ref_kemas.kemasan,tb_header.mtuang as mt_uang');
+        $this->db->select('tb_header.*,customer.*,SUM(tb_detail.pcs) AS pcs,SUM(tb_detail.kgs) AS kgs,tb_header.id AS idx,ref_kemas.kemasan,tb_header.mtuang AS mt_uang');
+        $this->db->from('tb_header');
         $this->db->join('customer', 'customer.id = tb_header.id_buyer', 'left');
-        $this->db->join('tb_detail', 'tb_detail.id_header = tb_header.id', 'left');
+
+
+        if ($jnsbc == '261') {
+            $this->db->join('tb_detail', 'tb_detail.id_akb = tb_header.id', 'left');
+        } elseif ($jnsbc == 'Y') {
+            $this->db->join('tb_detail', '(tb_detail.id_header = tb_header.id OR tb_detail.id_akb = tb_header.id)', 'left');
+        } else {
+            $this->db->join('tb_detail', 'tb_detail.id_header = tb_header.id', 'left');
+        }
+
+
         $this->db->join('ref_kemas', 'ref_kemas.kdkem = tb_header.kd_kemasan', 'left');
-        $this->db->where("tgl_bc between '" . tglmysql($tglawal) . "' AND '" . tglmysql($tglakhir) . "' ");
-        $this->db->where('trim(nomor_bc) !=', '');
+
+        $this->db->where("tgl_bc BETWEEN '" . tglmysql($tglawal) . "' AND '" . tglmysql($tglakhir) . "'");
+        $this->db->where("TRIM(nomor_bc) !=", '');
+
         if ($jnsbc != 'Y') {
             $this->db->where("jns_bc", $jnsbc);
         } else {
             $this->db->where_in("jns_bc", [25, 30, 261, 41]);
         }
+
         if ($this->session->userdata('nopen')) {
             $this->db->where("nomor_bc", $this->session->userdata('nopen'));
         }
+
         $this->db->where('data_ok', 1);
         $this->db->where('ok_tuju', 1);
-        $this->db->where('ok_valid', 1);
+        // $this->db->where('ok_valid', 1);
+
         $this->db->group_by('nomor_bc');
-        return $this->db->get('tb_header');
+
+        return $this->db->get();
     }
+
     public function getdatabyid($id)
     {
         $this->db->select('tb_header.*,customer.*,sum(tb_detail.pcs) as pcs,sum(tb_detail.kgs) as kgs,tb_header.id as idx,ref_jns_angkutan.angkutan as xangkutan,ref_mt_uang.mt_uang as xmt_uang,ref_kemas.kemasan');
@@ -38,15 +56,26 @@ class bckeluarmodel extends CI_Model
         $this->db->where('tb_header.id', $id);
         return $this->db->get('tb_header');
     }
+
+
     public function getdetailbyid($id)
     {
+        $header = $this->db->get_where('tb_header', ['id' => $id])->row_array();
+
         $this->db->select('tb_detail.*,satuan.kodesatuan,barang.nama_barang,barang.kode,tb_po.spek,tb_klppo.engklp');
+        $this->db->from('tb_detail');
         $this->db->join('satuan', 'satuan.id = tb_detail.id_satuan', 'left');
         $this->db->join('barang', 'barang.id = tb_detail.id_barang', 'left');
-        $this->db->join('tb_po', 'tb_po.po = tb_detail.po and tb_po.item = tb_detail.item and tb_po.dis = tb_detail.dis', 'left');
+        $this->db->join('tb_po', 'tb_po.po = tb_detail.po AND tb_po.item = tb_detail.item AND tb_po.dis = tb_detail.dis', 'left');
         $this->db->join('tb_klppo', 'tb_klppo.id = tb_po.klppo', 'left');
-        $this->db->where('tb_detail.id_header', $id);
-        return $this->db->get('tb_detail');
+
+        if ($header && $header['jns_bc'] == '261') {
+            $this->db->where('tb_detail.id_akb', $id);
+        } else {
+            $this->db->where('tb_detail.id_header', $id);
+        }
+
+        return $this->db->get();
     }
 
 
