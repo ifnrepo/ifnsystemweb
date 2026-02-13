a7bc2a095561409e3b6b6f450f448f091f608b1d
kelvin kusmawan <kelvinkusmawan12@gmail.com>
2026-02-13 16:43:25 +0700

    ui_rfidcontainer

--- application/controllers/Rfid_out.php
+++ application/controllers/Rfid_out.php
@@ -31,6 +31,9 @@ class Rfid_out extends CI_Controller
         // $footer['fungsi'] = 'container';
         $footer['data'] = $this->helpermodel->getdatafooter()->row_array();
         $data['filter_pl'] = $this->session->userdata('filter_pl') ?? 'all';
+        $data['filter_exdo'] = $this->session->userdata('filter_exdo') ?? 'all';
+        $data['filter_cekmasuk'] = $this->session->userdata('filter_cekmasuk') ?? 'all';
+        $data['filter_selesai'] = $this->session->userdata('filter_selesai') ?? 'all';
         $this->load->view('layouts/header', $header);
         $this->load->view('rfid_out/index', $data);
         $this->load->view('layouts/footer', $footer);
@@ -39,28 +42,57 @@ class Rfid_out extends CI_Controller
     public function filter_data()
     {
         $filter_pl = $this->input->post('filter');
+        $filter_exdo = $this->input->post('filter_exdo');
+        $filter_cekmasuk = $this->input->post('filter_cekmasuk');
+        $filter_selesai = $this->input->post('filter_selesai');
 
         $this->session->set_userdata('filter_pl', $filter_pl);
+        $this->session->set_userdata('filter_exdo', $filter_exdo);
+        $this->session->set_userdata('filter_cekmasuk', $filter_cekmasuk);
+        $this->session->set_userdata('filter_selesai', $filter_selesai);
 
-        $limit = $this->input->post('length');
-        $start = $this->input->post('start');
-        $draw = $this->input->post('draw');
+        $limit  = $this->input->post('length');
+        $start  = $this->input->post('start');
+        $draw   = $this->input->post('draw');
         $search = $this->input->post('search')['value'];
 
-        $this->db->select('tb_balenumber.*, tb_packfin.nw');
+
+        $this->db->select('tb_balenumber.*, tb_packfin.nw, tb_packfin.pcs, tb_packfin.meas , tb_po.spek');
         $this->db->from('tb_balenumber');
+
         $this->db->join(
             'tb_packfin',
             'tb_packfin.po = tb_balenumber.po 
-             AND tb_packfin.item = tb_balenumber.item
-             AND tb_packfin.nobale = tb_balenumber.nobale',
+         AND tb_packfin.item = tb_balenumber.item
+         AND tb_packfin.nobale = tb_balenumber.nobale',
             'left'
         );
+
+        $this->db->join(
+            'tb_po',
+            'tb_po.po = tb_balenumber.po AND tb_po.item = tb_balenumber.item',
+            'left'
+        );
+
 
 
         if ($filter_pl !== 'all' && !empty($filter_pl)) {
             $this->db->where('tb_balenumber.plno', $filter_pl);
         }
+        if ($filter_exdo !== 'all' && !empty($filter_exdo)) {
+            $this->db->where('tb_balenumber.exdo', $filter_exdo);
+        }
+        if ($filter_cekmasuk !== 'all' && !empty($filter_cekmasuk)) {
+            $this->db->where('tb_balenumber.masuk', $filter_cekmasuk);
+        }
+        // if ($filter_selesai !== 'all' && $filter_selesai !== null && $filter_selesai !== '') {
+        //     $this->db->where('tb_balenumber.selesai', $filter_selesai);
+        // }
+        if ($filter_selesai != 'all') {
+            $this->db->where('tb_balenumber.selesai', $filter_selesai);
+        }
+
+
 
 
         if (!empty($search)) {
@@ -72,54 +104,107 @@ class Rfid_out extends CI_Controller
 
         $this->db->order_by('tb_balenumber.id', 'ASC');
         $this->db->limit($limit, $start);
+
         $data = $this->db->get()->result_array();
 
         $no = $start + 1;
         foreach ($data as &$row) {
+
             $row['no'] = $no++;
             $row['plno'] = $row['plno'];
             $row['po'] = $row['po'];
+            $row['spek'] = "<span style='color:red; font-size:10px;'>{$row['exdo']}</span>
+                        <br>
+                        <span>{$row['spek']}</span>";
+
+            $row['pcs']   =  number_format((float)$row['pcs'], 2);
             $row['item'] = $row['item'];
             $row['nobale'] = $row['nobale'];
-            $row['berat'] = $row['nw'];
-            $selesai = $row['selesai'];
-            $status = '';
+            $row['berat'] = number_format((float)$row['nw'], 2);
+            $row['meas'] = $row['meas'];
+            $cek_masuk = $row['masuk'];
 
-            if ($selesai == 0) {
-                $status = '<div class="p-2 rounded" style="background:#fff3cd; border-left:4px solid #ffc107; font-size:12px;">
-                <strong><i class="fa fa-spinner fa-spin"></i> Waiting..</strong><br>
-              </div>';
+            if (empty($cek_masuk)) {
+                $row['masuk'] = '
+                <div class="p-2 rounded" style="background:#f8d7da; border-left:4px solid #dc3545; font-size:12px;">
+                    <strong><i class="fa fa-spinner fa-spin"></i> Verify..</strong><br>
+                </div>
+            ';
             } else {
-                $status = '<div class="p-2 rounded" style="background:#d4edda; border-left:4px solid #28a745; font-size:12px;">
-                <strong><i class="fa fa-check-circle"></i> Selesai</strong><br>
-                <span>Dicek Oleh: <b>' . ucwords(strtolower($row['user_ok'])) . '</b></span><br>
-                <span>Pada Tgl: ' . format_tanggal_indonesia_waktu($row['waktu_ok']) . '</span>
-              </div>';
+                $row['masuk'] = '
+                <div class="p-2 rounded" style="background:#fff3cd; border-left:4px solid #ffc107; font-size:10px;">
+                    <strong "><i class="fa fa-check-circle"></i>Noted ' . ucwords(strtolower($row['user_ok'])) . '</b></strong><br>
+                    <span style ="font-size:8px;">' . format_tanggal_indonesia_waktu($row['waktu_ok']) . '</span>
+                </div>
+            ';
             }
 
-            $row['selesai'] = $status;
+            $cek_selesai = $row['selesai'];
+
+            if ($cek_masuk == 1) {
+                if ($cek_selesai == 0) {
+                    $row['selesai'] = '
+                <div class="p-2 rounded" style="background:#fff3cd; border-left:4px solid #ffc107; font-size:12px;">
+                    <strong><i class="fa fa-spinner fa-spin"></i> Waiting..</strong><br>
+                </div>
+            ';
+                } else {
+                    $row['selesai'] = '
+                <div class="p-2 rounded" style="background:#d4edda; border-left:4px solid #28a745; font-size:12px;">
+                    <strong><i class="fa fa-check-circle"></i> Complite</strong><br>
+                </div>
+            ';
+                }
+            } else {
+
+                $row['selesai'] = '-';
+            }
 
             $row['aksi'] = '';
+            if (empty($cek_masuk)) {
+                $row['aksi'] = '
+                <a class="btn btn-sm btn-yellow btn-icon text-dark"
+                href="' . base_url('rfid_out/verifikasi/' . $row['id']) . '">
+                <i class="fa fa-check"></i>
+                 </a>
+            ';
+            }
 
-            if ($selesai == 0) {
-                $row['aksi'] =  '
-                <a class="btn btn-sm btn-info btn-icon text-dark verifikasi"
-                    data-id="' . $row['id'] . '"
-                    data-url="' . base_url() . 'rfid_out/verifikasi/' . $row['id'] . '" href="#">
-                    <i class="fa fa-check"></i>
-                </a>';
+
+            if ($cek_masuk == 1  && $cek_selesai == 0) {
+                $row['aksi'] = '
+                <a class="btn btn-sm btn-success btn-icon text-dark"
+                href="' . base_url('rfid_out/verifikasi_selesai/' . $row['id']) . '">
+                <i class="fa fa-check"></i>  <i class="fa fa-check"></i>
+                 </a>
+            ';
             }
         }
 
-        $this->db->select('tb_balenumber.*, tb_packfin.nw');
+
         $this->db->from('tb_balenumber');
         $this->db->join(
             'tb_packfin',
             'tb_packfin.po = tb_balenumber.po 
-             AND tb_packfin.item = tb_balenumber.item
-             AND tb_packfin.nobale = tb_balenumber.nobale',
+         AND tb_packfin.item = tb_balenumber.item
+         AND tb_packfin.nobale = tb_balenumber.nobale',
             'left'
         );
+
+
+        if ($filter_pl !== 'all' && !empty($filter_pl)) {
+            $this->db->where('tb_balenumber.plno', $filter_pl);
+        }
+
+        if ($filter_exdo !== 'all' && !empty($filter_exdo)) {
+            $this->db->where('tb_balenumber.exdo', $filter_exdo);
+        }
+        if ($filter_cekmasuk !== 'all' && !empty($filter_cekmasuk)) {
+            $this->db->where('tb_balenumber.masuk', $filter_cekmasuk);
+        }
+        if ($filter_selesai != 'all') {
+            $this->db->where('tb_balenumber.selesai', $filter_selesai);
+        }
 
 
         if (!empty($search)) {
@@ -132,21 +217,154 @@ class Rfid_out extends CI_Controller
         $recordsFiltered = $this->db->count_all_results();
 
 
+
         $this->db->from('tb_balenumber');
         $recordsTotal = $this->db->count_all_results();
 
+        // sum nw
+
+        $this->db->select('SUM(tb_packfin.nw) as total_nw');
+        $this->db->from('tb_balenumber');
+
+        $this->db->join(
+            'tb_packfin',
+            'tb_packfin.po = tb_balenumber.po 
+         AND tb_packfin.item = tb_balenumber.item
+         AND tb_packfin.nobale = tb_balenumber.nobale',
+            'left'
+        );
+
+
+        if ($filter_pl !== 'all' && !empty($filter_pl)) {
+            $this->db->where('tb_balenumber.plno', $filter_pl);
+        }
+
+        if ($filter_exdo !== 'all' && !empty($filter_exdo)) {
+            $this->db->where('tb_balenumber.exdo', $filter_exdo);
+        }
+        if ($filter_cekmasuk !== 'all' && !empty($filter_cekmasuk)) {
+            $this->db->where('tb_balenumber.masuk', $filter_cekmasuk);
+        }
+
+        if ($filter_selesai != 'all') {
+            $this->db->where('tb_balenumber.selesai', $filter_selesai);
+        }
+
+        if (!empty($search)) {
+            $this->db->group_start();
+            $this->db->like('tb_balenumber.plno', $search);
+            $this->db->or_like('tb_balenumber.po', $search);
+            $this->db->group_end();
+        }
+
+        $total_nw = $this->db->get()->row()->total_nw;
+        $total_nw = $total_nw ? $total_nw : 0;
+
+        // SUM PCS
+        $this->db->select('SUM(tb_packfin.pcs) as total_pcs');
+        $this->db->from('tb_balenumber');
+
+        $this->db->join(
+            'tb_packfin',
+            'tb_packfin.po = tb_balenumber.po 
+         AND tb_packfin.item = tb_balenumber.item
+         AND tb_packfin.nobale = tb_balenumber.nobale',
+            'left'
+        );
+
+
+        if ($filter_pl !== 'all' && !empty($filter_pl)) {
+            $this->db->where('tb_balenumber.plno', $filter_pl);
+        }
+
+        if ($filter_exdo !== 'all' && !empty($filter_exdo)) {
+            $this->db->where('tb_balenumber.exdo', $filter_exdo);
+        }
+        if ($filter_cekmasuk !== 'all' && !empty($filter_cekmasuk)) {
+            $this->db->where('tb_balenumber.masuk', $filter_cekmasuk);
+        }
+
+        if ($filter_selesai != 'all') {
+            $this->db->where('tb_balenumber.selesai', $filter_selesai);
+        }
+
+        if (!empty($search)) {
+            $this->db->group_start();
+            $this->db->like('tb_balenumber.plno', $search);
+            $this->db->or_like('tb_balenumber.po', $search);
+            $this->db->group_end();
+        }
+
+        $total_pcs = $this->db->get()->row()->total_pcs;
+        $total_pcs = $total_pcs ? $total_pcs : 0;
+
+        // SUM MEAS
+        $this->db->select('SUM(tb_packfin.meas) as total_meas');
+        $this->db->from('tb_balenumber');
+
+        $this->db->join(
+            'tb_packfin',
+            'tb_packfin.po = tb_balenumber.po 
+         AND tb_packfin.item = tb_balenumber.item
+         AND tb_packfin.nobale = tb_balenumber.nobale',
+            'left'
+        );
+
+
+        if ($filter_pl !== 'all' && !empty($filter_pl)) {
+            $this->db->where('tb_balenumber.plno', $filter_pl);
+        }
+
+        if ($filter_exdo !== 'all' && !empty($filter_exdo)) {
+            $this->db->where('tb_balenumber.exdo', $filter_exdo);
+        }
+        if ($filter_cekmasuk !== 'all' && !empty($filter_cekmasuk)) {
+            $this->db->where('tb_balenumber.masuk', $filter_cekmasuk);
+        }
+        if ($filter_selesai != 'all') {
+            $this->db->where('tb_balenumber.selesai', $filter_selesai);
+        }
+
+
+        if (!empty($search)) {
+            $this->db->group_start();
+            $this->db->like('tb_balenumber.plno', $search);
+            $this->db->or_like('tb_balenumber.po', $search);
+            $this->db->group_end();
+        }
+
+        $total_meas = $this->db->get()->row()->total_meas;
+        $total_meas = $total_meas ? $total_meas : 0;
+
+
 
         echo json_encode([
-            'draw' => intval($draw),
-            'recordsTotal' => $recordsTotal,
+            'draw'            => intval($draw),
+            'recordsTotal'    => $recordsTotal,
             'recordsFiltered' => $recordsFiltered,
-            'data' => $data
+            'total_nw'        =>  number_format((float)$total_nw, 2),
+            'total_pcs'        =>  number_format((float)$total_pcs, 2),
+            'total_meas'        =>  $total_meas,
+            'data'            => $data
         ]);
     }
+
+
+
 
     public function verifikasi($id)
     {
         $update = $this->Rfid_outmodel->verifikasi_data($id);
+        if ($update) {
+            $this->session->set_flashdata('success', 'Data berhasil diverifikasi!');
+        } else {
+            $this->session->set_flashdata('error', 'Gagal melakukan verifikasi data.');
+        }
+        redirect('rfid_out');
+    }
+    public function verifikasi_selesai($id)
+    {
+        $update = $this->Rfid_outmodel->verifikasi_selesai($id);
         if ($update) {
             $this->session->set_flashdata('success', 'Data berhasil diverifikasi!');
         } else {
